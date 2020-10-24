6a20d238a55c90b191b09064fd89f5e776273602
Liam Allan <liam.allan@freschesolutions.com>
2020-10-24 15:25:29 +0100

    Use env file when debugging

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const connection = require('./lib/driver/Connection');
 
 (async () => {
-  const conn = await connection.getConnection(false, "", "", "");
+  const conn = await connection.getConnection(false, process.env.SYSTEM, process.env.USER, process.env.PASS);
 })();
