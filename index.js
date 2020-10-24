63d0f2323c0c96956c191d678587a2f277d1c93c
Liam Allan <liam.allan@freschesolutions.com>
2020-10-24 18:16:12 +0100

    We have a valid statement being created

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const connection = require('./lib/driver/Connection');
 
 (async () => {
   const conn = await connection.getConnection(false, process.env.SYSTEM, process.env.USER, process.env.PASS);
+
+  const stmt = await conn.createStatement();
+
+  conn.close();
 })();
